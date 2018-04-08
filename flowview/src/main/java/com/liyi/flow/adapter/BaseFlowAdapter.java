eba5c614d4a0e73a1ad332a8f2e0d65cc9a0e0d1
albertlii <albertlii@163.com>
2018-04-08 14:28:25 +0800

    上传新的示例图

--- flowview/src/main/java/com/liyi/flow/adapter/BaseFlowAdapter.java
+++ flowview/src/main/java/com/liyi/flow/adapter/BaseFlowAdapter.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
  * FlowView 的适配器的基类
  */
 public abstract class BaseFlowAdapter {
+
     public abstract View getView(int position, View convertView, ViewGroup parent);
 
     public abstract int getCount();
